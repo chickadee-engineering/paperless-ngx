9fb0fedb7aa0b66a9efae300bd3fcb3ca8e23e4b
shamoon <4887959+shamoon@users.noreply.github.com>
2023-06-01 02:36:06 -0700

    input component unit tests

--- src-ui/jest.config.js
+++ src-ui/jest.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/e2e/', 'abstract*'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/e2e/',
+    'abstract-name-filter-service',
+    'abstract-paperless-service',
+  ],
   transformIgnorePatterns: [`<rootDir>/node_modules/(?!.*\\.mjs$|lodash-es)`],
   moduleNameMapper: {
     '^src/(.*)': '<rootDir>/src/$1',
